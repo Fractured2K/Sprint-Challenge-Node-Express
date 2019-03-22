936d1a89bdc42f3f9fe432ff312368ebe68c1ac3
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-22 08:16:40 -0700

    Scaffold API

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-// play this: https://www.youtube.com/watch?v=d-diB65scQU
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const cors = require("cors");
 
-// code away!
+// endpoints
+const projectController = require("./controllers/projects");
+const actionController = require("./controllers/actions");
+
+const server = express();
+
+// middelware
+server.use(express.json());
+server.use(bodyParser.json());
+server.use(cors());
+server.use(helmet());
+
+// route handlers
+server.use("/api/projects", projectController);
+server.use("/api/actions", actionController);
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+	console.log(`Server now running on http://localhost:${port}`);
+});
