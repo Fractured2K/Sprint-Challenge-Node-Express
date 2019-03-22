7deb2a6449c4ffb3014041730568867ca17e94c3
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-22 08:22:14 -0700

    Handle get projects endpoint

--- controllers/projects.js
+++ controllers/projects.js
@@ -2,6 +2,21 @@ const router = require("express").Router();
 
 const Project = require("../data/helpers/projectModel");
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+	try {
+		const projects = await Project.get();
+
+		if (!projects)
+			return res
+				.status(404)
+				.json({ message: "Sorry, no projects were found" });
+
+		return res.status(200).json(projects);
+	} catch (err) {
+		res.status(500).json({
+			message: "Sorry, we counldn't get any projects"
+		});
+	}
+});
 
 module.exports = router;
