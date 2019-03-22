b9c6ded42b5bbb85f1ef5670b8b5a89f079acfc6
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-22 08:24:39 -0700

    Handle get project by id endpoint

--- controllers/projects.js
+++ controllers/projects.js
@@ -21,6 +21,23 @@ router.get("/", async (req, res) => {
 });
 
 // Get project by id
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		const projects = await Project.get(id);
+
+		if (!projects)
+			return res
+				.status(404)
+				.json({ message: "Sorry, that project doesn't exist" });
+
+		return res.status(200).json(projects);
+	} catch (err) {
+		res.status(500).json({
+			message: "Sorry, we counldn't get that project"
+		});
+	}
+});
 
 module.exports = router;
