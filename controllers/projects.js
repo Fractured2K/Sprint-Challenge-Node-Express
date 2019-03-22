3574943a063168080ed6867497ed23e9824c1a94
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-22 08:36:11 -0700

    Handle delete project endpoint

--- controllers/projects.js
+++ controllers/projects.js
@@ -76,6 +76,23 @@ router.put("/:id", async (req, res) => {
 });
 
 // Delete project
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		const project = await Project.remove(id);
+
+		if (!project)
+			return res
+				.status(404)
+				.json({ message: "Sorry, that project doesn't exist" });
+
+		return res.status(200).json(project);
+	} catch (err) {
+		res.status(500).json({
+			message: "Sorry, there was an error trying to delete that project"
+		});
+	}
+});
 
 module.exports = router;
