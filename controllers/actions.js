623a9177ba14a5035d072b71f01cfe9a452b7ce2
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-22 08:59:34 -0700

    Define get action by id

--- controllers/actions.js
+++ controllers/actions.js
@@ -21,11 +21,6 @@ router.get("/", async (req, res) => {
 	try {
 		const actions = await Action.get();
 
-		if (!actions)
-			return res
-				.status(404)
-				.json({ message: "Sorry, no actions were found" });
-
 		return res.status(200).json(actions);
 	} catch (err) {
 		res.status(500).json({
@@ -33,5 +28,8 @@ router.get("/", async (req, res) => {
 		});
 	}
 });
+
+// Get action by id
+router.get("/:id", async (req, res) => {});
 
 module.exports = router;
--- controllers/projects.js
+++ controllers/projects.js
@@ -22,11 +22,6 @@ router.get("/", async (req, res) => {
 	try {
 		const projects = await Project.get();
 
-		if (!projects)
-			return res
-				.status(404)
-				.json({ message: "Sorry, no projects were found" });
-
 		return res.status(200).json(projects);
 	} catch (err) {
 		res.status(500).json({
@@ -41,16 +36,10 @@ router.get("/:id", async (req, res) => {
 		const { id } = req.params;
 
 		const projects = await Project.get(id);
-
-		if (!projects)
-			return res
-				.status(404)
-				.json({ message: "Sorry, that project doesn't exist" });
-
 		return res.status(200).json(projects);
 	} catch (err) {
 		res.status(500).json({
-			message: "Sorry, we counldn't get that project"
+			message: "Sorry, we couldn't get that project"
 		});
 	}
 });
