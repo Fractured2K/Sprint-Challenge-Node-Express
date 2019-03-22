d292e2299c66e8aa30ebfb456d9c0055235625d3
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-22 08:42:40 -0700

    Handle create action endpoint

--- controllers/actions.js
+++ controllers/actions.js
@@ -1,5 +1,18 @@
 const router = require("express").Router();
 
-router.post("/", async (req, res) => {});
+const Action = require("../data/helpers/actionModel");
+
+router.post("/", async (req, res) => {
+	try {
+		const action = req.body;
+
+		const newAction = await Action.insert(action);
+		res.status(201).json(newAction);
+	} catch (err) {
+		res.status(500).json({
+			message: "Sorry, there was an error creating that action"
+		});
+	}
+});
 
 module.exports = router;
