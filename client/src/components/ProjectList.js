49ac82b6193a5235169854cad5d47e777e30fc75
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-22 10:01:22 -0700

    ProjectList component map state

--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
+import axios from "axios";
 
+// components
+import Project from "./Project";
 class ProjectList extends Component {
-	componentDidMount = () => {};
+	state = {
+		projects: []
+	};
+	componentDidMount = () => {
+		axios
+			.get("http://localhost:3001/api/projects")
+			.then(res =>
+				this.setState({
+					projects: res.data
+				})
+			)
+			.catch(err => {
+				console.log(err);
+			});
+	};
 
 	render() {
 		return (
 			<div>
 				<h1>hello</h1>
+				{this.state.projects.map(project => (
+					<Project {...project} key={project.id} />
+				))}
 			</div>
 		);
 	}
