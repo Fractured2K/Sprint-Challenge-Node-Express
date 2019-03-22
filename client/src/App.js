4d774f7be03bb0175659eb908d14dc8cc2041214
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-22 09:55:45 -0700

    Scaffold ProjectList

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import ProjectList from "./components/ProjectList";
+
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>Hello</h1>
+				<ProjectList />
 			</div>
 		);
 	}
---
+++ client/src/components/ProjectList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class ProjectList extends Component {
+	componentDidMount = () => {};
+
+	render() {
+		return (
+			<div>
+				<h1>hello</h1>
+			</div>
+		);
+	}
+}
+
+export default ProjectList;
